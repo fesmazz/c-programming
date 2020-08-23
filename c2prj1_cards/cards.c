67242c7a425e9c88b0b13ed1872ca0a2b0b76446
Coursera Learner <nobody@nowhere.nul>
2020-08-23 07:44:20 +0000

    vr1

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,96 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  assert(c.value >= 2 && c.value <= VALUE_ACE);
+  assert(c.suit >= SPADES && c.suit <= CLUBS);
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
+  switch (r) {
+  case STRAIGHT_FLUSH: printf("STRAIGHT_FLUSH\n"); break;
+  case FOUR_OF_A_KIND: printf("FOUR_OF_A_KIND\n"); break;
+  case FULL_HOUSE: printf("FULL_HOUSE\n"); break;
+  case FLUSH: printf("FLUSH\n"); break;
+  case STRAIGHT: printf("STRAIGHT\n"); break;
+  case THREE_OF_A_KIND: printf("THREE_OF_A_KIND\n"); break;
+  case TWO_PAIR: printf("TWO_PAIR\n"); break;
+  case PAIR: printf("PAIR\n"); break;
+  case NOTHING: printf("NOTHING\n"); break;
+  }
   return "";
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  char x;
+  switch (c.value) {
+  case 2: x = 50; break;
+  case 3: x = 51; break;
+  case 4: x = 52; break;
+  case 5: x = 53; break;
+  case 6: x = 54; break;
+  case 7: x = 55; break;
+  case 8: x = 56; break;
+  case 9: x = 57; break;
+  case 10: x = 48; break;
+  case 11: x = 74; break;
+  case 12: x = 81; break;
+  case 13: x = 75; break;
+  case 14: x = 65; break;
+  }		      
+		 
+  return x;
 }
 
 
 char suit_letter(card_t c) {
-  return 'x';
+  char x;
+  switch (c.suit) {
+  case SPADES: x = 115; break;
+  case HEARTS: x = 104; break;
+  case DIAMONDS: x = 100; break;
+  case CLUBS: x = 99; break;
+  case NUM_SUITS: printf("invalid suit. Exiting..."); exit (EXIT_FAILURE);
+  }
+  return x;
   
 }
-
 void print_card(card_t c) {
-
+  char value = value_letter(c);
+  char suit = suit_letter(c);
+  printf("%c%c", value, suit);
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  if (((value_let >= 50 && value_let <= 57) || value_let == 48 || value_let == 74 || value_let == 81 || value_let == 75 || value_let == 65) && (suit_let == 99 || suit_let == 100 || suit_let == 104 || suit_let == 115)){
+    temp.value = value_let;
+    temp.suit = suit_let;
+  }
+    else {
+      printf("Card entered is not valid. Exiting...");
+      exit(EXIT_FAILURE);
+    }
+  
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  if (c >= 0 && c < 52) {
+  temp.value = c % 13 + 2;
+    if (c >= 0 && c < 13) {
+    temp.suit = SPADES;
+    }
+    if (c >= 13 && c < 26) {
+    temp.suit = HEARTS;
+    }
+    if (c >= 26 && c < 39) {
+    temp.suit = DIAMONDS;
+    }
+    if (c >= 39 && c < 52) {
+    temp.suit = CLUBS;
+    }
+  }
+  
   return temp;
 }
