b2df66e1882674f42646db4ec7cdb57eb7b2c396
Coursera Learner <nobody@nowhere.nul>
2020-09-01 20:14:47 +0000

    assign 19

--- 19_bits_arr/numToBits.c
+++ 19_bits_arr/numToBits.c
@@ -36,7 +36,15 @@ int getNthBit(uint32_t number, int bit) {
  *   be bit 30 of nums[0], and so on.
  */
 void numToBits(uint32_t * nums, int nNums, int * bits, int nBits) {
-
+  if (nNums * 32 > nBits) {
+    printf("Invalid call to numToBits! nBits is %d, nNums is %d\n", nBits, nNums);
+    return;
+  }
+  for (int i = 0; i < nNums; i++) {  // começa a acessar os elementos de nums
+    for (int j = 0;  j < 32;  j++) {   // começa a extrair os bits de nums[i]
+      bits[32*i + j] = getNthBit(nums[i], 31-j);
+    }
+  }
 }
 
 void doTest(uint32_t * nums, int n) {
