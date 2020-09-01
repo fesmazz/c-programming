e68078981e2ac1da804feadc7ea00aa1f76f4c03
Coursera Learner <nobody@nowhere.nul>
2020-09-01 22:53:05 +0000

    fixed rotate.c

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -3,14 +3,16 @@
 
 void rotate (char matrix[10] [10]) {
   char temp[10] [10];
-  for (int line = 0; line < 10; line++) {
+  for (int line = 0; line < 10; line++) {                // cria uma copia de matrix em temp rotacionada 90º
     for (int column = 0; column < 10; column++) {
-      temp[line] [9-column] = matrix[line] [column];
+      temp[line] [9-column] = matrix[line] [column];     
     }
   }
-  for (int line = 0; line < 10; line++) {
-    strcpy (matrix[line], temp[line]);
-      }
+  for (int line = 0; line < 10; line++) {               //copia temp para matrix
+     for (int column = 0; column < 10; column++) {
+       matrix[line] [column] = temp[line] [column];
+     }
+  }
   return;
 }
 
