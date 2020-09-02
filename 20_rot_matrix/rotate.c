6cf2d4160e9ebe1aa0326e48bedc7c07c10af692
Coursera Learner <nobody@nowhere.nul>
2020-09-02 00:13:53 +0000

    fixed conversion formula

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 void rotate (char matrix[10] [10]) {
   char temp[10] [10];
   for (int line = 0; line < 10; line++) {                // cria uma copia de matrix em temp rotacionada 90º
     for (int column = 0; column < 10; column++) {
-      temp[line] [9-column] = matrix[line] [column];     
+      temp[column] [9-line] = matrix[line] [column];     
     }
   }
   for (int line = 0; line < 10; line++) {               //copia temp para matrix
