d905ef82550db139fa5f95a11f671f8217c31624
Fernando Mazzilli <fernandosmazzilli@gmail.com>
2020-09-11 19:14:34 -0300

    rotateMatrix.c

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -2,34 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-char ** write_input_to_arr(FILE * input, char ** arr, size_t line_lenght, size_t n_lines) {
-  arr = (int **) arr; //convertendo tipo do array para aceitar todos os caracteres ASCI
-  for (int i = 0; i < n_columns; i++) {
-    if (i > n_lines) {
-      fprintf(stderr, "Input file has too many lines!", i);
-      exit (EXIT_FAILURE);
-    }
-    int line[line_lenght];   
-    while (fgets(line, line_lenght, input) != NULL) {
-      
-      if (strchr(line, '\n') == NULL) {
-	fprintf(stderr, "Line %d on input file is too long.", i);
-	exit (EXIT_FAILURE);
-      }
-      if (*(strchr(line, '\n')) != line[line_lenght-1]) {
-	fprintf(stderr, "Line %d on input file is too short.", i);
-	exit (EXIT_FAILURE);
-      }
-      strcpy(arr[i][0], line[0]);
+void check_characters(FILE * input) {
+  int c;
+  while ((c = fgetc(input)) != EOF) {
+    if (c > 255) {
+	fprintf(stderr, "Invalid characters detected on file");
+      	exit (EXIT_FAILURE);
     }
   }
-  arr = (char **) arr; // convertendo array de volta para o tipo original
-  return arr; 
+  rewind(input);
+}
+
+void  write_input_to_arr(FILE * input, char arr[10][12], size_t lines, size_t line_lenght) {
+  int i = 0;
+  char line[line_lenght]; 
+  while (fgets(line, line_lenght, input) != NULL) {
+    if (i > lines) {
+      fprintf(stderr, "Input file has too many lines!");
+      exit (EXIT_FAILURE);
+    }    
+    if (strchr(line, '\n') == NULL) {
+      	fprintf(stderr, "Line %d on input file is too long.\n", i);
+      	exit (EXIT_FAILURE);
+       }
+     if (strchr(line, '\n') != & line[10]) {
+     	fprintf(stderr, "Line %d on input file is too short.\n", i);
+     	exit (EXIT_FAILURE);
+      }
+    strcpy(arr[i], line);
+    i++;
+  }
+  // print_matrix(arr, lines, line_lenght); 
 }
 
 
-void rotate (char ** matrix) {
-  char temp[10] [10];
+void rotate (char matrix[10][12]) {
+  char temp[10] [11];
   for (int line = 0; line < 10; line++) {                // cria uma copia de matrix em temp rotacionada 90º
     for (int column = 0; column < 10; column++) {
       temp[column] [9-line] = matrix[line] [column];
@@ -43,8 +51,10 @@ void rotate (char ** matrix) {
   return;
 }
  
- void print_matrix (char ** matrix, size_t line_lenght, size_t n_columns) {
-
+ void print_matrix (char matrix [10][12], size_t lines) {   
+     for (int i = 0; i < lines; i++) {
+       fprintf(stdout, "%s", matrix[i]);
+     }	        
  }
  
 
@@ -55,12 +65,13 @@ int main (int argc, char ** argv) {
   }
   FILE * input_file = fopen(argv[1], "r");
   if (input_file == NULL) {
-    fprintf(stderr, "Could not open file: %s", argv[1]);
+    fprintf(stderr, "Could not open file: %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
-  char matrix [10][11];
-  write_input_to_arr(input_file, matrix, 11, 10);
+  char matrix [10][12];
+  check_characters(input_file);
+  write_input_to_arr(input_file, matrix, 10, 12);
   rotate(matrix);
-  print_matrix(matrix);
+  print_matrix(matrix, 10);
   return 0;  
 }
