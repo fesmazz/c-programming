67562e37610d7a449c1dedfaceea71663008e0ac
Fernando Mazzilli <fernandosmazzilli@gmail.com>
2020-09-14 00:26:34 -0300

    fixed encrypt.c

--- 28_fix_vg_encr/encrypt.c
+++ 28_fix_vg_encr/encrypt.c
@@ -4,10 +4,10 @@
 #include <string.h>
 
 void encrypt(FILE * f, int key, FILE * outfile){
-  char * line;
-  size_t sz;
+  char * line = NULL;
+  size_t sz = 0;
   while (getline(&line,&sz, f) >= 0) {
-    char * ptr = line;
+    char * ptr = line;    
     while (*ptr != '\0') {
       int c = *ptr;
       if (isalpha(c)) {
@@ -20,8 +20,9 @@ void encrypt(FILE * f, int key, FILE * outfile){
       *ptr = c;
       ptr++;
     }
-    fprintf(outfile, "%s", line);
+    fprintf(outfile, "%s", ptr);
   }
+  free(line);
 }
 
 int main(int argc, char ** argv) {
@@ -40,7 +41,7 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   //outfileNAme is argv[2] + ".txt", so add 4 to its length.
-  char * outFileName = malloc((strlen(argv[2]) + 4) * sizeof(*outFileName));
+  char * outFileName = malloc((strlen(argv[2]) + 5) * sizeof(*outFileName));
   strcpy(outFileName, argv[2]);
   strcat(outFileName, ".enc");
   FILE * outFile = fopen(outFileName, "w");
