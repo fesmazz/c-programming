19698209ea2f3fa02a55ee154519074a29d9ed63
Fernando Mazzilli <fernandosmazzilli@gmail.com>
2020-09-14 21:47:15 -0300

    wrote main

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -17,8 +17,40 @@ void sortData(char ** data, size_t count) {
 }
 
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+  char ** lines = NULL;
+  char * curr_line = NULL;
+  size_t sz = 0;
+  size_t line_count = 0;
+  if (argc == 1) {
+    while (getline(&curr_line, &sz, stdin) >= 0) {
+      lines = realloc(lines, (line_count + 1) * sizeof(*lines));
+      lines[line_count] = malloc(sz * sizeof(**lines));
+      strcpy(lines[line_count], curr_line);
+      line_count++;
+    }
+  }
+  else {
+    for (int i = 1; i <= argc-1; i++) {
+      FILE * input = fopen(argv[i], "r");
+      if (input == NULL) {
+         fprintf(stderr, "Error opening file: %s", argv[i]);
+         exit(EXIT_FAILURE);
+      }
+       while (getline(&curr_line, &sz, input) >= 0) {
+        lines = realloc(lines, (line_count + 1) * sizeof(*lines));
+        lines[line_count] = malloc(sz * sizeof(**lines));
+        strcpy(lines[line_count], curr_line);
+        line_count++;
+      }
+      fclose(input);
+    } 
+  }
+  free(curr_line);
+  sortData(lines, line_count);
+  for (int i = 0; i < line_count; i++) {
+    fprintf(stdout, "%s", lines[i]);
+    free(lines[i]);
+  }
+  free (lines);  
   return EXIT_SUCCESS;
 }
