c984b81cf5418b1b1814350cc259250b0db411d1
Fernando Mazzilli <fernandosmazzilli@gmail.com>
2020-09-28 03:32:48 -0300

    Vr 1 of main. Doesn't handle ties well enough yet

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -11,6 +11,105 @@
 
 
 int main(int argc, char ** argv) {
-  //YOUR CODE GOES HERE
+
+  if (argc < 2 || argc > 3) {
+    fprintf(stderr, "Usage: %s input.txt numTrials\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  size_t num_trials = 10000; 
+
+  if (argc == 3) {
+    char * check_num;
+    size_t temp = strtol(argv[2], &check_num, 0); //converte argv[2] para uma long em base 10
+    if (check_num == NULL) {
+      num_trials = temp;
+    }
+    else {
+      fprintf(stderr, "numTrials must be a number. Exiting...\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+/* Esta porção do código é responsável por abrir o arquivo indicado na execução do programa, definir e iniciliazar
+as variáveis responsáveis por armazenar as informações obtidas, e criar as variáveis de trabalho do programa. */
+
+  FILE * input = fopen(argv[1], "r");
+  if (input == NULL) {
+    fprintf(stderr, "Failed to open file %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+    }
+
+  future_cards_t fc;
+    fc.decks = NULL;
+    fc.n_decks = 0;
+
+  size_t n_hands = 0;
+
+  deck_t ** hands = read_input(input, &n_hands, &fc);
+  
+  deck_t * remaining_deck = build_remaining_deck(hands, n_hands);
+
+  size_t win_count[n_hands+1]; 
+  for (size_t i = 0; i <= n_hands+1; i++) {
+    win_count[i] = 0;
+  }
+
+// Esta porção do código roda as simulações
+
+  if (n_hands < 2) {
+    fprintf(stderr, "At least 2 hands are needed to make a comparison\n");
+    exit(EXIT_FAILURE);
+  }
+  for (size_t i = 0; i < num_trials; i++) {
+    shuffle(remaining_deck);
+    future_cards_from_deck(remaining_deck, &fc);
+    int best_hand = 0;
+    int curr_hand = 0;
+    int tie = 0;
+    for (int i = 0; i < n_hands; i++) {
+      if (compare_hands(hands[i], hands[curr_hand]) == 1) {
+        curr_hand = i;
+        if (compare_hands(hands[curr_hand], hands[best_hand]) == 1) {
+          best_hand = i;
+        }
+      }
+    }
+    for (int i = 0; i < n_hands; i++) {
+      if (i == best_hand) {break;}
+      if (compare_hands(hands[i], hands[best_hand]) == 0 ) { // caso de empate
+        tie++;
+      }
+    }
+  if (tie > 0) {
+    win_count[n_hands+1]++;
+  }
+  else {
+    win_count[best_hand]++;
+    }
+  } 
+// Esta porção do código exibe os resultados 
+
+  for (size_t i = 0; i < n_hands; i++) {
+    float ratio = (float) win_count[i] / (float) num_trials * 100;
+    fprintf(stdout, "Hand %zu won %lu / %lu times (%.2f%%)\n", i, win_count[i], num_trials, ratio);
+  }
+  fprintf(stdout, "And there were %lu ties\n", win_count[n_hands+1]);
+  
+//fechando arquivos e liberando memória alocada
+
+  if (fclose(input) != 0) { 
+    fprintf(stderr, "Error closing file %s\n", argv[1]);
+    }
+
+  for(size_t i = 0; i < fc.n_decks; i++) {
+        free(fc.decks[i].cards);
+    }
+  free (fc.decks);
+
+  for(size_t i = 0; i < n_hands; i++) {
+        free_deck(hands[i]);
+    }
+
   return EXIT_SUCCESS;
 }
